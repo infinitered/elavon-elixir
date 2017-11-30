89a15fb34184bc33859d8bea741e005ea13b4c5b
Zachary Berkompas <zberkom@gmail.com>
2017-11-30 13:17:54 -0800

    Bumb version number

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Elavon.Mixfile do
     [
       app: :elavon,
       description: "Native elixir client for USBank Elavon Converge API",
-      version: "0.1.0",
+      version: "0.1.1",
       elixir: "~> 1.5",
       start_permanent: Mix.env == :prod,
       deps: deps(),
